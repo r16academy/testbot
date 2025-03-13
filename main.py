6676d6f11035f0d81dc84db1159da96cfe741eb1
r16academy <r16academy@gmail.com>
2025-03-13 06:31:34 +0530

    Test commit.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# test from internetdsf
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def hello_world():
+    return "<h1>Hello, APIs hit!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
